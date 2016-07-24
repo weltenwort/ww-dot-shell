ffe3ced0292235b16018e8717171beb8a7a4c03b
Felix Stürmer <stuermer@weltenwort.de>
2016-07-24 16:22:14 +0200

    Clone and pull zgen

--- ellipsis.sh
+++ ellipsis.sh
@@ -2,18 +2,22 @@
 #
 # weltenwort/ww-dot-shell ellipsis package
 
+readonly ZGEN_PATH="${HOME}/.zgen"
+
 # The following hooks can be defined to customize behavior of your package:
 pkg.install() {
-    git.clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
+    git.clone https://github.com/tarjoilija/zgen.git "${ZGEN_PATH}"
 }
 
 # pkg.push() {
 #     git.push
 # }
 
-# pkg.pull() {
-#     git.pull
-# }
+pkg.pull() {
+    hooks.pull
+
+    git.pull "${ZGEN_PATH}"
+}
 
 # pkg.installed() {
 #     git.status
